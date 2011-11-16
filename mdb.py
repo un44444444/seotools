b02b11cf1e3b3afcaa8af441f0cd8daa13bdad01
un44444444 <un44444444@gmail.com>
2011-11-17 02:57:46 +0800

    add little code

--- mdb.py
+++ mdb.py
@@ -11,8 +11,10 @@ class AdoMdb:
 		try:
 			constr = 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=%s' % filepath
 			#print constr
-			#tell the server  we are not planning to update...
-			adodbapi.adodbapi.defaultIsolationLevel = adodbapi.adodbapi.adXactBrowse
+			#tell the server we are not planning to update...
+			#adodbapi.adodbapi.defaultIsolationLevel = adodbapi.adodbapi.adXactBrowse
+			#we are planning to update...
+			adodbapi.adodbapi.defaultIsolationLevel = adodbapi.adodbapi.adXactReadCommitted
 			#and we want a local cursor (so that we will have an accurate rowcount)
 			adodbapi.adodbapi.defaultCursorLocation = adodbapi.adodbapi.adUseClient
 			self.conn=adodbapi.connect(constr)
@@ -36,16 +38,31 @@ class AdoMdb:
 			print 'Get ColName error'
 			return []
 		return self.colnamelist
-
-	def QueryTable(self,sql,showraw=100):
+	
+	def GetRecordCount(self,sql):
+		try:
+			self.cur.execute(sql)
+			return self.cur.rowcount
+		except:
+			print 'Get record count error'
+		return 0
+	
+	def ExecuteQuery(self,sql,showraw=100):
 		records = []
 		try:
 			self.cur.execute(sql)
-			print 'rowcount =',self.cur.rowcount
+			#print 'rowcount =',self.cur.rowcount
 			records = self.cur.fetchmany(showraw)
 		except:
-			print 'Query table error'
+			print 'Execute query table error'
 		return records
+	
+	def ExecuteUpdate(self,sql):
+		try:
+			self.cur.execute(sql)
+			self.conn.commit()
+		except:
+			print 'Execute update table error'
 	
 	def CloseDatabase(self):
 		try:
@@ -60,13 +77,15 @@ if __name__ == '__main__':
 	db.OpenDataBase('R:/SpiderResult.mdb')
 	#list=db.GetAllColName('content')
 	#print list
-	db = db.QueryTable(u'select ID,标题 from content', 2)
-	for i in db:
+	resultset = db.ExecuteQuery(u'select ID,标题 from content', 2)
+	for record in resultset:
 		result=""
-		for j in i:
+		for j in record:
 			if isinstance(j, unicode):
 				j=j.encode('cp936')
 			elif not isinstance(j, str):
 				j=str(j)
 			result=result+j+' '
 		print result
+	#update
+	db.ExecuteUpdate(u"update content set 标题='custom title by python' where ID=2")
--- poster.py
+++ poster.py
@@ -13,9 +13,24 @@ def post_data(action, data):
 	#except:
 	#	return 'except'
 
+def get_data(action):
+	#try:
+		req=urllib2.Request(action)
+		resp=urllib2.urlopen(req)
+		content=resp.read()
+		return content
+	#except:
+	#	return 'except'
+
 if __name__ == '__main__':
-	action = 'http://api.spinnerchief.com:8080/?apikey=45a0a23babbd44729&username=un44444444&password=44444444'
+	apikey = '45a0a23babbd44729'
+	username = 'un44444444'
+	password = '44444444'
+	action = 'http://api.spinnerchief.com:9001/?apikey=%s&username=%s&password=%s' % (apikey, username, password)
+	action = action + '&spintype=1&spinfreq=1&original=1'
+	#action = action + '&querytimes=2'
 	print action
-	data = 'Hello, what is your name?'
+	data = "Because of cubic zirconia's low cost, high quality, and close visual likeness to diamond, cubic zirconia is also known as cz, zirconia and man made diamonds, the best substitute of natural diamonds. synthetic cubic zirconia has remained the most gemologically and economically important competitor for diamonds since 1976.shoponchina is the direct factory sources for cubic zirconia, We have over 25 years of experience in the cubic zirconia wholesale business. Our value is our knowledge and tenure in helping our customers succeed."
+	#data = ''
 	result = post_data(action, data)
 	print result
