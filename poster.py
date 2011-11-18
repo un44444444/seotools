7251a7bf660575ab5d58d5de71943fad63d61948
un44444444 <un44444444@gmail.com>
2011-11-19 00:28:45 +0800

    a runnable version

--- poster.py
+++ poster.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python
 # encoding: UTF-8
 
-import urllib,urllib2
+import urllib2
 
-def post_data(action, data):
-	#try:
-		#data = urllib.urlencode(data)
-		req=urllib2.Request(action,data)
-		resp=urllib2.urlopen(req)
-		content=resp.read()
-		return content
-	#except:
-	#	return 'except'
-
-def get_data(action):
-	#try:
-		req=urllib2.Request(action)
-		resp=urllib2.urlopen(req)
-		content=resp.read()
-		return content
-	#except:
-	#	return 'except'
+class Poster:
+	def __init__(self):
+		port = 8080
+		apikey = '45a0a23babbd44729'
+		username = 'un44444444'
+		password = '44444444'
+		action = 'http://api.spinnerchief.com:%d/?apikey=%s&username=%s&password=%s' % (port, apikey, username, password)
+		self.action_spin = action + '&spintype=1&spinfreq=1&original=1&protecthtml=1&protectwords=cubic,zirconia,wholesale,rings,earring,gemstone,synthetic'
+		self.action_times = action + '&querytimes=2'
+	
+	def spin_content(self, data):
+		return self._post_data(self.action_spin, data)
+	
+	def query_times(self):
+		return self._post_data(self.action_times, '')
+	
+	@staticmethod
+	def _post_data(action, data):
+		try:
+			req=urllib2.Request(action,data)
+			resp=urllib2.urlopen(req)
+			content=resp.read()
+			return content
+		except:
+			return 'except'
+	
+	@staticmethod
+	def _get_data(action):
+		try:
+			req=urllib2.Request(action)
+			resp=urllib2.urlopen(req)
+			content=resp.read()
+			return content
+		except:
+			return 'except'
 
 if __name__ == '__main__':
-	apikey = '45a0a23babbd44729'
-	username = 'un44444444'
-	password = '44444444'
-	action = 'http://api.spinnerchief.com:9001/?apikey=%s&username=%s&password=%s' % (apikey, username, password)
-	action = action + '&spintype=1&spinfreq=1&original=1'
-	#action = action + '&querytimes=2'
-	print action
-	data = "Because of cubic zirconia's low cost, high quality, and close visual likeness to diamond, cubic zirconia is also known as cz, zirconia and man made diamonds, the best substitute of natural diamonds. synthetic cubic zirconia has remained the most gemologically and economically important competitor for diamonds since 1976.shoponchina is the direct factory sources for cubic zirconia, We have over 25 years of experience in the cubic zirconia wholesale business. Our value is our knowledge and tenure in helping our customers succeed."
-	#data = ''
-	result = post_data(action, data)
+	poster = Poster()
+	data = "<p><p><p>Cubic Zirconia, it was in the form of poly-crystalline ceramic, used as a refractory material. The cubic zirconia was perfectly invented in former Soviet Union in 1973 and the mass commercial production began in 1976. It became popular in 1980 and had reached 10 tons of global production.<p>Cubic Zirconia is known as low cost synthetic diamond which is close visual likeness to diamond and it was founded since 1976. The synthesized material is relatively hard, slightly harder than the most semi-precious gems. Its dispersion is very high, even higher than diamond. It is usually colorless (It may be made in different vary colors) but it change to other colors under shortwave or longwave UV such as yellow, greenish yellow, beige and whitish.<p>In recent years, the jewelry manufacturers have sought ways to improve cubic zirconia. The CZ's coating of diamond-like carbon is one of such innovation. The resulting material overall is harder, more lustrous and more like diamond. Its refractive index is improved thus making it appear more like diamond.<p>Today, you may easily shop around your favorite cubic zirconia jewelry through online jewelry stores and there are lots choices in terms of categories, patterns, styles as well as prices. Affordable prices on fashion jewelry, costume jewelry, jewelry accessories, all at Casjewellery.com <br>http://www.casjewellery.com/newshow-135.html<br>www.CASJewellery.com has become a global costume jewelry wholesaler since 2008; we serve customers all over the world and the growth still ascending. Whether you are a wholesaler or a shopper looking for the latest products, we are treating impartially to offer the same lowest price to all. For years, we had never changed our faith, to provide experience professional services and maintain the products in high quality standard are our most important task to achieve.<p>My business scope include a variety product range of body piercing jewelry , pendants, bead jewelry, rhinestone jewelry, pearl jewelry. Cubic zirconia jewelry costume jewelry. Simultaneously we maintain good and stable co operations with our manufacturers and therefore our price is definitively competitive in the market. You do not have to worry and time wasting to search the products, all you need to do is enter to our online purchase website. <p>Our corporate culture: coordination, cooperation, enthusiasm and sincere. <p>Business advantages: superior quality, innovative style, low price, which won a high recognition from our customers. All products are taken the same in pictures, so you can be assured of the quality of our products.<p>Business objectives: We do our utmost to cooperate with our partners to establish a close and mutually beneficial relationship base on equality thus to build up a good reputation and credibility to win the utmost trusty. <p>Enterprise¡¯s goal: We do the best to establish a closely, mutually beneficial relations with our co-operator and to win a good prestige and good faith as well.<br>Enterprise¡¯s idea: Sincere, mutual benefits. Our customer supporting is our best motivation. We rigid to provide the most satisfied products and the service conscientiously to all our customers.<br>"
+	result = poster.spin_content(data)
+	print result
+	import time
+	time.sleep(1)
+	result = poster.query_times()
 	print result
---
+++ spinner.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+def addslashes(s):
+	#d = {"'":"\\'", "\0":"\\\0", "\\":"\\\\"}
+	d = {"'":"''"}
+	return ''.join(d.get(c, c) for c in s if c<chr(128))
+
+if __name__ == '__main__':
+	import time
+	import mdb
+	import poster
+	spinner = poster.Poster()
+	db = mdb.AdoMdb()
+	db.OpenDataBase('R:/SpiderResult.mdb')
+	#
+	resultset = db.ExecuteQuery(u'select ID,标题,内容 from content WHERE spin=0', 500)
+	for record in resultset:
+		result=[0,'','']
+		idx = 0
+		for j in record:
+			if isinstance(j, unicode):
+				#result[idx]=j.encode('cp936')
+				result[idx]=j
+			else:
+				result[idx]=j
+			idx+=1
+		#print result
+		
+		#title
+		time.sleep(1)
+		title = spinner.spin_content(result[1])
+		if title == 'except':
+			print "title except"
+			continue
+		print result[1]
+		print title
+		
+		#content
+		time.sleep(1)
+		content = spinner.spin_content(result[2])
+		if content == 'except':
+			print "content except"
+			continue
+		#print result[2]
+		#print content
+		
+		#update
+		title = addslashes(title)
+		content = addslashes(content).encode('utf8')
+		db.ExecuteUpdate(u"update content set 标题='%s',内容='%s',spin=1 where ID=%d" % (title, content, result[0]))
+		print 'complete ID=%d' % result[0]
+	db.CloseDatabase()
+	print spinner.query_times()
