2d0925d1a344723cbd98403441a7f232faf2de8d
un44444444 <un44444444@gmail.com>
2011-11-28 01:09:34 +0800

    little change on account succeed

--- poster.py
+++ poster.py
@@ -6,19 +6,31 @@ import time
 
 class Poster:
 	def __init__(self):
+		self.index = -1
+		self._change_action()
+		
+	def _change_action(self):
 		host = ''.join(['http://api.','spinner','chief','.com'])
 		port = 8080
 		config = [
 				('45a0a23babbd44729', ''.join(['un','4444','4444']), '44444444'),
 				('753a2d8b9995450e8', ''.join(['ray','mond','182']), '2198300')
 		]
-		(apikey,username,password) = config[0]
+		if self.index >= (len(config)-1):
+			return False
+		self.index += 1
+		(apikey,username,password) = config[self.index]
 		action = '%s:%d/?apikey=%s&username=%s&password=%s' % (host, port, apikey, username, password)
 		self.action_spin = action + '&spintype=1&spinfreq=1&original=1&protecthtml=1&protectwords=cubic,zirconia,wholesale,rings,earring,gemstone,synthetic'
 		self.action_times = action + '&querytimes=2'
+		return True
 	
 	def spin_content(self, data):
-		return self._post_data(self.action_spin, data)
+		result = self._post_data(self.action_spin, data)
+		if result == 'error=You have reached the daily limit!':
+			if self._change_action():
+				result = self._post_data(self.action_spin, data)
+		return result
 	
 	def query_times(self):
 		return self._post_data(self.action_times, '')
