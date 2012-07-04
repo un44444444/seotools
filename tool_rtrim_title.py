f2f4a6aae05ec3a84ebfa826edf1d962fabce1f3
un44444444 <un44444444@gmail.com>
2012-07-05 02:03:04 +0800

    little enhence on tool_rtrim_title.py

--- tool_rtrim_title.py
+++ tool_rtrim_title.py
@@ -12,11 +12,12 @@ def addslashes(s):
 if __name__ == '__main__':
 	import mdb
 	db = mdb.AdoMdb()
-	db.OpenDataBase('R:/#%20c5dba49d4810cff95595.mdb')
+	db.OpenDataBase('R:/#%20963e007635a3753a011f.mdb')
 	#
 	last_time = None
-	max_fixtime_id = 1
-	resultset = db.ExecuteQuery(u'SELECT log_id,log_title,log_posttime FROM blog_Article WHERE log_id>1', 500)
+	resultset = db.ExecuteQuery(u'SELECT Min(log_id) FROM blog_Article', 1)
+	max_fixtime_id = int(resultset[0][0])
+	resultset = db.ExecuteQuery(u'SELECT log_id,log_title,log_posttime FROM blog_Article WHERE log_id>%d'%max_fixtime_id, 500)
 	for record in resultset:
 		atricle_id = int(record[0])
 		atricle_title = record[1]
@@ -31,9 +32,9 @@ if __name__ == '__main__':
 		print 'begin new Title=%s' % atricle_title
 		#time determine update
 		if last_time is not None and (atricle_time-last_time).days<1:
-			db.ExecuteUpdate(u"UPDATE blog_Article SET log_title='%s',log_intro='',log_posttime=log_posttime+%d WHERE log_id=%d" % (atricle_title, atricle_id-max_fixtime_id, atricle_id))
+			db.ExecuteUpdate(u"UPDATE blog_Article SET log_title='%s',log_intro='',log_posttime=log_posttime+%d WHERE log_id=%d" % (atricle_title, atricle_id-max_fixtime_id+1, atricle_id))
 		else:
-			db.ExecuteUpdate(u"UPDATE blog_Article SET log_title='%s',log_intro='' WHERE log_id=%d" % (atricle_title, atricle_id))
+			db.ExecuteUpdate(u"UPDATE blog_Article SET log_title='%s',log_intro='',log_posttime=log_posttime+1 WHERE log_id=%d" % (atricle_title, atricle_id))
 			max_fixtime_id = atricle_id
 		last_time = atricle_time
 		print 'complete ID=%d' % atricle_id
